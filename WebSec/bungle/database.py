a8d720ac37807b840e7539f7af199d6818ff1763
ubuntu <ubuntu@ubuntu.(none)>
2019-09-21 17:18:18 -0400

    2.1.1-3 done

--- WebSec/bungle/database.py
+++ WebSec/bungle/database.py
@@ -11,9 +11,9 @@ def connect():
     #TODO: fill out function parameters. Use the user/password combo for the user you created in 2.1.2.1
 
     return mdb.connect(host="localhost",
-                       user="",
-                       passwd="",
-                       db="");
+                       user="mirecki2",
+                       passwd="a19e87d10737bc2e10f62a9f8857fdb6da8f37ebb8f28cc5c1a2bf2aedb927bf",
+                       db="project2");
 
 def createUser(username, password):
     """ creates a row in table named users
@@ -23,7 +23,15 @@ def createUser(username, password):
 
     db_rw = connect()
     cur = db_rw.cursor()
+
     #TODO: Implement a prepared statement using cur.execute() so that this query creates a row in table user
+    insert_stmt = (
+    "INSERT INTO users(username,password,passwordhash) "
+    "VALUES(%s,%s,%s)"
+    )
+    insert_data = (username, password, md5)
+    cur.execute(insert_stmt, insert_data)
+
     db_rw.commit()
 
 def validateUser(username, password):
@@ -36,6 +44,14 @@ def validateUser(username, password):
     db_rw = connect()
     cur = db_rw.cursor()
     #TODO: Implement a prepared statement using cur.execute() so that this query selects a row from table user
+    insert_stmt = (
+    "SELECT username, password "
+    "FROM users "
+    "WHERE username=%s AND password=%s"
+    )
+    insert_data = (username, password)
+    cur.execute(insert_stmt, insert_data)
+
     if cur.rowcount < 1:
         return False
     return True
@@ -52,6 +68,14 @@ def fetchUser(username):
     cur = db_rw.cursor(mdb.cursors.DictCursor)
     print username
     #TODO: Implement a prepared statement so that this query selects a id and username of the row which has column username = username
+    insert_stmt = (
+    "SELECT id, username "
+    "FROM users "
+    "WHERE username=%s"
+    )
+    insert_data = (username,)
+    cur.execute(insert_stmt, insert_data)
+
     if cur.rowcount < 1:
         return None
     return FormsDict(cur.fetchone())
@@ -65,6 +89,13 @@ def addHistory(user_id, query):
     db_rw = connect()
     cur = db_rw.cursor()
     #TODO: Implement a prepared statment using cur.execute() so that this query inserts a row in table history
+    insert_stmt = (
+    "INSERT INTO history(user_id,query) "
+    "VALUES(%s,%s)"
+    )
+    insert_data = (user_id, query)
+    cur.execute(insert_stmt, insert_data)
+
     db_rw.commit()
 
 def getHistory(user_id):
@@ -77,5 +108,15 @@ def getHistory(user_id):
     db_rw = connect()
     cur = db_rw.cursor()
     #TODO: Implement a prepared statement using cur.execute() so that this query selects 15 queries from table history in descending order of when the searches were made
+    insert_stmt = (
+    "SELECT query "
+    "FROM history "
+    "WHERE user_id=%s "
+    "ORDER BY id DESC "
+    "LIMIT 15"
+    )
+    insert_data = (user_id,)
+    cur.execute(insert_stmt, insert_data)
+
     rows = cur.fetchall();
     return [row[0] for row in rows]

